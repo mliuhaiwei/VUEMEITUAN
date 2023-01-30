b304b02db95e02321656799af39f501d3cd96f88
harvey <mliuhaiwei@gmail.com>
2023-01-30 13:32:09 +0800

    VueMeituan

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://open.duyiedu.com'
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
-  console.log(config)
   config.params = {
     ...config.params,
     appkey: 'asdf_1572851470835'
